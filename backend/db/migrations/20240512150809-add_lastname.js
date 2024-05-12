a06dde0a1f649771723ad238046a546c6d613be7
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-05-12 11:09:42 -0400

    update migration

--- backend/db/migrations/20240512150809-add_lastname.js
+++ backend/db/migrations/20240512150809-add_lastname.js
@@ -1,23 +1,18 @@
 'use strict';
-
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', { 
+    await queryInterface.addColumn('Users', 'lastName', { 
       
       type: Sequelize.STRING(30)},options);
-
-      
   },
 
   async down (queryInterface, Sequelize) {
     options.tableName = "Users";
-    await queryInterface.removeColumn(options, 'firstName');
-    
+    await queryInterface.removeColumn(options, 'lastName')
   }
 };
