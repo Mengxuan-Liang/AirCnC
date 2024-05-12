8a7f8045712bc2f31fae72985fcffdab5829e3b6
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-05-12 11:00:09 -0400

    modify migration

--- backend/db/migrations/20240512140943-add_first_last_name_col.js
+++ backend/db/migrations/20240512140943-add_first_last_name_col.js
@@ -1,18 +1,25 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('Users', 'firstName', { 
       
-      type: Sequelize.STRING(30), });
+      type: Sequelize.STRING(30)},options);
+
       await queryInterface.addColumn('Users', 'lastName', { 
       
-        type: Sequelize.STRING(30), });
+        type: Sequelize.STRING(30)},options);
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName')
+    options.tableName = "Users";
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName')
   }
 };
